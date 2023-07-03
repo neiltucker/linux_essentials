9e735e3663e2718f977ab2a521f74418ded1353e
Neil Tucker <49943805+neiltucker@users.noreply.github.com>
2023-07-02 20:24:11 -0400

    Add files via upload

---
+++ create-csv.bat
@@ -0,0 +1,8 @@
+echo ID,FirstName,LastName > employees.csv
+echo 1001,John,Williams >> employees.csv
+echo 1002,Bill,Brown >> employees.csv
+echo 1003,Carol,Young >> employees.csv
+echo 1004,Harry,Hunter >> employees.csv
+echo 1005,Hazel,Smith >> employees.csv
+echo 1006,Tony,Thomas >> employees.csv
+
