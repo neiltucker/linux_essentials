daf3db68a062303fe1d722374db7fe29ffcd4efe
Neil Tucker <49943805+neiltucker@users.noreply.github.com>
2024-10-17 09:54:51 -0400

    Add files via upload

--- ubuntu24.sh
+++ ubuntu24.sh
@@ -1,12 +1,12 @@
 ### Setup for local LLM on Ubuntu
 
-# Prep system
+## Prep system
 sudo apt-get update && sudo apt-get -y upgrade && apt-get autoremove && apt-get autoclean
 
-# Git
+## Git
 sudo apt install git-all -y
 
-# Docker
+## Docker
 for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
 sudo apt-get install ca-certificates curl -y
 sudo install -m 0755 -d /etc/apt/keyrings
@@ -17,6 +17,11 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
+sudo apt-get update -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+# sudo docker run hello-world
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+## Download Ollama
+curl -fsSL https://ollama.com/install.sh | sh
+
+
