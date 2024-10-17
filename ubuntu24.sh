bba7627d7f2e4667c97daf97009428f24fc036a2
Neil Tucker <49943805+neiltucker@users.noreply.github.com>
2024-10-17 09:46:23 -0400

    Add files via upload

--- ubuntu24.sh
+++ ubuntu24.sh
@@ -1,4 +1,20 @@
+### Setup for local LLM on Ubuntu
+
+# Prep system
 sudo apt-get update && sudo apt-get -y upgrade && apt-get autoremove && apt-get autoclean
+
+# Git
 sudo apt install git-all -y
 
+# Docker
 for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+sudo apt-get install ca-certificates curl -y
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
