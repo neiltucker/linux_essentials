37f7010a668df4177f60c85d2005c572962daaf0
Neil Tucker <49943805+neiltucker@users.noreply.github.com>
2024-10-17 21:44:25 -0400

    Update create-csv.sh

--- create-csv.sh
+++ create-csv.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 # curl https://raw.githubusercontent.com/neiltucker/linux_essentials/main/create-csv.sh | sh
-
 echo "ID,FirstName,LastName" > employees.csv
 echo "1001,John,Williams" >> employees.csv
 echo "1002,Bill,Brown" >> employees.csv
