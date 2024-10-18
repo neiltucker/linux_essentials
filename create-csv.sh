407cfa9330684546f2745526519bbeeba9394675
Neil Tucker <49943805+neiltucker@users.noreply.github.com>
2024-10-17 21:43:21 -0400

    Update create-csv.sh

--- create-csv.sh
+++ create-csv.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # curl https://raw.githubusercontent.com/neiltucker/linux_essentials/main/create-csv.sh | sh
-# Create an employees records comma delimited file (ID, FirstName, LastName)
+
 echo "ID,FirstName,LastName" > employees.csv
 echo "1001,John,Williams" >> employees.csv
 echo "1002,Bill,Brown" >> employees.csv
