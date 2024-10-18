143fdb835c654ef17d29cc55adaf6bc0855dc32d
Neil Tucker <49943805+neiltucker@users.noreply.github.com>
2024-10-17 21:30:04 -0400

    Update create-csv.sh

--- create-csv.sh
+++ create-csv.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # curl -sSL https://raw.githubusercontent.com/neiltucker/linux_essentials/main/create-csv.sh | sh
 
-echo "ID,FirstName,LastName" > ./employees.csv
-echo "1001,John,Williams" >> ./employees.csv
-echo "1002,Bill,Brown" >> ./employees.csv
-echo "1003,Carol,Young" >> ./employees.csv
-echo "1004,Harry,Hunter" >> ./employees.csv
-echo "1005,Hazel,Smith" >> ./employees.csv
-echo "1006,Tony,Thomas" >> ./employees.csv
+echo "ID,FirstName,LastName" > employees.csv
+echo "1001,John,Williams" >> employees.csv
+echo "1002,Bill,Brown" >> employees.csv
+echo "1003,Carol,Young" >> employees.csv
+echo "1004,Harry,Hunter" >> employees.csv
+echo "1005,Hazel,Smith" >> employees.csv
+echo "1006,Tony,Thomas" >> employees.csv
 echo "employees.csv file has been created successfully."
